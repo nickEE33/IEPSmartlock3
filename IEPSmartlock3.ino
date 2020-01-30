8cb8d3e170a2d8ad097f31f7db46f06693f59191
shteay <shteay@gmail.com>
2020-01-30 14:56:44 +0000

    Changed project name.

--- IEPSmartlock3.ino
+++ IEPSmartlock3.ino
@@ -1,26 +1,21 @@
-
 #include <Wire.h>
 #include <LIS3MDL.h>
 
 LIS3MDL mag;
 
-
 char report[80];
-
-
 
 void setup()
 {
   Serial.begin(115200);
   Wire.begin(13, 16);
   
-
   if (!mag.init())
   {
     Serial.println("Failed to detect and initialize magnetometer!");
     while (1);
   }
-
+  
   mag.enableDefault();
  
 }
@@ -31,9 +26,9 @@ void loop()
 
   snprintf(report, sizeof(report), "M: %6d %6d %6d",
            mag.m.x, mag.m.y, mag.m.z);
- Serial.println(report);
- // float re = (mag.m.x ^ 2 + mag.m.y ^ 2 + mag.m.z ^ 2) ^ 1 / 2;
- // Serial.println(re);
+  Serial.println(report);
+  // float re = (mag.m.x ^ 2 + mag.m.y ^ 2 + mag.m.z ^ 2) ^ 1 / 2;
+  // Serial.println(re);
 
 
   delay(100);
